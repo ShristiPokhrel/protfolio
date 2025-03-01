8cf7814bd9708104baf73242fc7c16f67dad11b7
ShristiPokhrel <shristipok2000@gmail.com>
2025-03-01 23:06:36 +0545

    Updated portfolio with 2025 version

--- style.js
+++ style.js
@@ -35,14 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Student", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: [ "QA Engineer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
